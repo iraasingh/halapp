cd61d153f69e760d0d04fcd0af1e07b88e74dcbe
Iraa Singh <iraasingh@Iraas-MacBook-Air.local>
2025-03-14 15:58:35 -0400

    Deploy Flutter web build to gh-pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4e6f5f1ac79a7deed6a840aff096df7c",
+const RESOURCES = {"flutter_bootstrap.js": "0a1918e084ebc0a6667a490fa3de991c",
 "version.json": "2b521e10dfa0f067561de489a19d6620",
 "index.html": "d0201075d149617fe3cbb8841ed93403",
 "/": "d0201075d149617fe3cbb8841ed93403",
