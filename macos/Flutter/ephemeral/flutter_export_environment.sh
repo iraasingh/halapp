c169966dc7082ac7ecd695b2356c64e5f9450150
Iraa Singh <iraasingh@Iraas-MacBook-Air.local>
2025-03-17 11:08:47 -0400

    Initial commit

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.27.3/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/iraasingh/App/frontend"
+export "FLUTTER_APPLICATION_PATH=/Users/iraasingh/App/frontend/halapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
